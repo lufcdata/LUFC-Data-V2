fb9b70d06a9fc1a7f749a57a4933aed22091d793
lufcdata <65512426+lufcdata@users.noreply.github.com>
2026-09-06 12:29:47 +0100

    Restore Stat Pack source from verified pre-edit blob

--- frontend/src/StatPack.tsx
+++ frontend/src/StatPack.tsx
@@ -3,5 +3,3 @@ import{supabase,supabaseConfigError}from'./supabase';
 import{researchUpcomingFixture}from'./statPackFixtureResearch';
 import{matchesExactFixtureScope}from'./statPackFixtureScope';
 import{canonicalStadiumKey,matchesAtPhysicalStadium}from'./stadiumIdentity';
-
-// RESTORE_FROM_BLOB_REQUIRED
